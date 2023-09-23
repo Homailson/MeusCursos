c68db5fd970ddb726bb04b2b053868ff952cd6c0
Homailson <homailson@gmail.com>
2023-09-23 18:50:19 -0300

    continuando curso Python / Flask

---
+++ Udemy/PythonFlask/Python_Concepts/29_imports_in_python/mymodule.py
@@ -0,0 +1,5 @@
+def divide(dividend, divisor):
+    return dividend / divisor
+
+print("mymodule.py: ", __name__) ## __name__ é uma variável em python que muda dependendo do arquivo em que você está.
+                                 ## através dela, podemos diferenciar o arquivo que você está rodando e o que está sendo importado.
