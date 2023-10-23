464d86e7003d441c09a8a4c82bd196b50dcef03c
Homailson <Homailson@Admins-MacBook-Pro.local>
2023-10-23 12:40:32 -0300

    continuando curso de python flask

--- Udemy/Portifolio/static/js/highlight.js
+++ Udemy/Portifolio/static/js/highlight.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {   
+document.addEventListener("DOMContentLoaded", function () {
     highlightCurrentSection();
 });
 
 function highlightCurrentSection() {
     var currentSection = getCurrentSection();
 
-    if (currentSection) {               
-        var sidebarLinks = document.querySelectorAll('.sidebar__item');
+    if (currentSection) {
+        var sidebarLinks = document.querySelectorAll('.sidebar__list__item');
         sidebarLinks.forEach(function (link) {
-            link.classList.remove('active-link');
+            link.classList.remove('sidebar__active__link');
         });
-        
+
         var currentSectionId = currentSection.id;
-                        
+
         var activeLink = document.querySelector('a[href="#' + currentSectionId + '"]');
         if (activeLink) {
-            activeLink.classList.add('active-link');
+            activeLink.classList.add('sidebar__active__link');
         }
     }
 }
