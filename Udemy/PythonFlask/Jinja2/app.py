c9a0a3555873bbf6ccdc1548e8c22c55d935d350
Homailson <homailson@gmail.com>
2023-11-02 11:48:20 -0300

    continuando curso de python com flask

--- Udemy/PythonFlask/Jinja2/app.py
+++ Udemy/PythonFlask/Jinja2/app.py
@@ -4,6 +4,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class CarrosBrasileiros:
+    def __init__(self, marca, modelo, ano, qtd_rodas, qtd_portas, qtd_lugares):
+        self.marca=marca
+        self.modelo=modelo
+        self.ano=ano
+        self.qtd_rodas=qtd_rodas
+        self.qtd_portas=qtd_portas
+        self.qtd_lugares=qtd_lugares
+
+
+
 @app.route("/data-structures/")
 def render_data_structures():
 
@@ -13,4 +24,54 @@ def render_data_structures():
         "A beautiful Mind"
     ]
 
-    return render_template("data_structures.html", movies=movies)
+    car = {
+        "brand": "Chevrolet",
+        "model": "Monza",
+        "year": "1990"
+    },
+
+    carro = CarrosBrasileiros("Gurgel", "BR-800", "1990", "4", "2", "4")
+
+    kwargs = {
+        "movies": movies,
+        "car": car,
+        "carro": carro
+    }
+ 
+
+    return render_template("data_structures.html", **kwargs)
+
+
+@app.route("/condicionais-basicas/")
+def render_conditional():
+    # company = "Apple"
+    company = "Microsoft"
+    return render_template("conditional_basics.html", company=company)
+
+
+@app.route("/for-loop/")
+def render_loops_for():
+    planets = [
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupter",
+        "Saturn",
+        "Uranus",
+        "Neptune"
+    ]
+
+    return render_template("for_loop.html", planets=planets)
+
+
+@app.route("/for-loop-condicionais/")
+def render_for_loop_conditionais():
+    user_os = {
+        "Bob_Smith": "Windows",
+        "Anne Pun": "MacOS",
+        "Adam Lee": "Linux",
+        "Jose Slvatierra": "Windows"
+    }
+
+    return render_template("loops_and_conditionals.html", user_os=user_os)
