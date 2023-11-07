bd66a6d662e004aa9a01f199fe50f103e39c3207
Homailson <homailson@gmail.com>
2023-11-07 15:36:51 -0300

    estrutura basica pronta

--- Udemy/Portfolio_v3/togglemenu.js
+++ Udemy/Portfolio_v3/togglemenu.js
@@ -1,15 +1,10 @@
-// When the item is activated by click, if the navList element has
-// the nav__list class it will loss it, othewise will gain it
 function toggleMenu() {
     var navList = document.getElementById("nav__list");
-    if (window.innerWidth < 768) {
-        navList.classList.toggle("nav__hidden");
+    if (window.innerWidth <= 768) {
+        if (navList.classList.contains('nav__hidden')) {
+            navList.classList.remove('nav__hidden');
+        } else {
+            navList.classList.add('nav__hidden');
+        }
     }
 }
-
-// If the screen is reduced to a size smaller than 768px add the class
-// nav__hidden to navList element, otherwise remove it
-window.addEventListener('resize', function () {
-    var navList = document.getElementById("nav__list");
-    navList.classList.toggle("nav__hidden", window.innerWidth < 768);
-});
