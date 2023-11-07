cb0d039d49695eaefbce7e4109221749d92afdcf
Homailson <Homailson@Admins-MacBook-Pro.local>
2023-11-07 11:00:25 -0300

    ajustada a funçao do menu recursivo

--- Udemy/Portfolio_v3/togglemenu.js
+++ Udemy/Portfolio_v3/togglemenu.js
@@ -1,19 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const navList = document.querySelector('.nav__list');
+// When the item is activated by click, if the navList element has
+// the nav__list class it will loss it, othewise will gain it
+function toggleMenu() {
+    var navList = document.getElementById("nav__list");
+    if (window.innerWidth < 768) {
+        navList.classList.toggle("nav__hidden");
+    }
+}
 
-    menuIcon.addEventListener('click', function () {
-        if (window.innerWidth <= 768) {
-            navList.style.display = (navList.style.display === 'none' || navList.style.display === '') ? 'block' : 'none';
-        }
-    });
-
-    const navLinks = document.querySelectorAll('.nav__list a');
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            if (window.innerWidth <= 768) {
-                navList.style.display = 'none';
-            }
-        });
-    });
+// If the screen i reduced to a size smaller than 768px add the class
+// nav__hidden to navList element, otherwise remove it
+window.addEventListener('resize', function () {
+    var navList = document.getElementById("nav__list");
+    navList.classList.toggle("nav__hidden", window.innerWidth < 768);
 });
