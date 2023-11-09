33268c1d8a7a260a6613521aeecf3646e9434b77
Homailson <Homailson@Admins-MacBook-Pro.local>
2023-11-08 21:38:00 -0300

    tudo bem ajustado

--- Udemy/Portfolio_v3/static/js/togglemenu.js
+++ Udemy/Portfolio_v3/static/js/togglemenu.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     var navList = document.getElementById("nav__list");
-    if (window.innerWidth <= 768) {
+    if (window.innerWidth <= 540) {
         if (navList.classList.contains('nav__hidden')) {
             navList.classList.remove('nav__hidden');
         } else {
